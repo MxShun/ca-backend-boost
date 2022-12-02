0ea46cb0649856bf49810a112c1d667e1df84416
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-02 16:41:24 +0900

    5

--- main.go
+++ main.go
@@ -1,64 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	num1 := scanNumber(1)
-
-	ope := scanOperator()
-
-	num2 := scanNumber(2)
-
-	fmt.Print(calc(num1, num2, ope))
+	n := 10
+	exec1(n)
+	n = 20
+	exec2(&n)
+	fmt.Printf("Finish : %d \n", n)
 }
 
-func scanNumber(index int) int {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Printf("Input %d:", index)
-	scanner.Scan()
-	strNum := scanner.Text()
-	num, err := strconv.Atoi(strNum)
-
-	if (err != nil) {
-		fmt.Println("正しい数値を入力してください")
-		return scanNumber(index)
-	}
-
-	return num
+func exec1(num int) {
+	fmt.Printf("exec1 : %d \n", num)
+	num = 100
+	fmt.Printf("exec1 : %d \n", num)
 }
 
-func scanOperator() string {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Print("InputOperator", ":")
-	scanner.Scan()
-	str := scanner.Text()
-
-	switch(str) {
-	case "+", "-", "*", "/":
-		return str
-	default:
-		fmt.Println("正しい演算子を入力してください")
-		return scanOperator()
-	}
-}
-
-func calc(num1, num2 int, ope string) int {
-	switch(ope) {
-	case "+":
-		return num1 + num2
-	case "-":
-		return num1 - num2
-	case "*":
-		return num1 * num2
-	case "/":
-		return num1 / num2
-	}
-	return 0
+func exec2(num *int) {
+	fmt.Printf("exec2 : %d \n", *num)
+	*num = 200
+	fmt.Printf("exec2 : %d \n", *num)
 }
