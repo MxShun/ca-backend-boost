e5ae227c86febfcbced67aa9f410b3fc50152f0a
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-02 16:30:03 +0900

    4

--- main.go
+++ main.go
@@ -9,18 +9,56 @@ import (
 
 func main() {
 	num1 := scanNumber(1)
+
+	ope := scanOperator()
+
 	num2 := scanNumber(2)
 
-	fmt.Print(num1 + num2)
+	fmt.Print(calc(num1, num2, ope))
 }
 
 func scanNumber(index int) int {
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Print("Input", index, ":")
+	fmt.Printf("Input %d:", index)
 	scanner.Scan()
 	strNum := scanner.Text()
-	num, _ := strconv.Atoi(strNum)
+	num, err := strconv.Atoi(strNum)
+
+	if (err != nil) {
+		fmt.Println("正しい数値を入力してください")
+		return scanNumber(index)
+	}
 
 	return num
+}
+
+func scanOperator() string {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("InputOperator", ":")
+	scanner.Scan()
+	str := scanner.Text()
+
+	switch(str) {
+	case "+", "-", "*", "/":
+		return str
+	default:
+		fmt.Println("正しい演算子を入力してください")
+		return scanOperator()
+	}
+}
+
+func calc(num1, num2 int, ope string) int {
+	switch(ope) {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		return num1 / num2
+	}
+	return 0
 }
