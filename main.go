e638ed53d26d35efe51506c217185ca0155532e5
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-09 16:37:55 +0900

    2-fixed

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ func main() {
 	var days = flag.Int("days", 0, "日数の入力")
 	var locale = flag.String("locale", "Asia/Tokyo", "ロケールの入力")
 
+	// NOTE: フラグの設定前でもダメだし、利用する後でのダメなのでこの位置
+	// https://zenn.dev/yshingo/articles/263f7fde177b1b
 	flag.Parse()
 
 	location, err := time.LoadLocation(*locale)
