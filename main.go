8d05d23ac929b0494e18885b9d208810e9d0f61e
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-09 16:11:51 +0900

    2

--- main.go
+++ main.go
@@ -1,36 +1,22 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
+	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"time"
 )
 
-type payload struct {
-	Text string `json:"text"`
-}
-
 func main() {
-	postUrl := ""
-	reqJson := payload{"5_ハロー世界."}
-	bJson, err := json.Marshal(reqJson)
+	var days = flag.Int("days", 0, "日数の入力")
+	var locale = flag.String("locale", "Asia/Tokyo", "ロケールの入力")
+
+	// flag.Parse()
+
+	location, err := time.LoadLocation(*locale)
 	if err != nil {
-		panic(err)
+			panic(err)
 	}
-	req, err := http.NewRequest(http.MethodPost, postUrl, bytes.NewBuffer(bJson))
-	if err != nil {
-		panic(err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	byteArray, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("%#v", string(byteArray))
+
+	var now = time.Now().In(location)
+	fmt.Println(now.AddDate(0, 0, *days))
 }
