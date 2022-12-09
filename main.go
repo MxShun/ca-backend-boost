b73b8f92561e1c07d64c84641afb4529c12eece6
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-09 16:52:33 +0900

    Revert "cobra-cli"
    This reverts commit b0f704ee2a313b373a5bb2f96901518939c7c16a.

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package main
 
-import "main/cmd"
+import (
+	"flag"
+	"fmt"
+	"time"
+)
 
 func main() {
-	cmd.Execute()
+	var days = flag.Int("days", 0, "日数の入力")
+	var locale = flag.String("locale", "Asia/Tokyo", "ロケールの入力")
+
+	// NOTE: フラグの設定前でもダメだし、利用する後でのダメなのでこの位置
+	// https://zenn.dev/yshingo/articles/263f7fde177b1b
+	flag.Parse()
+
+	location, err := time.LoadLocation(*locale)
+	if err != nil {
+		panic(err)
+	}
+
+	var now = time.Now().In(location)
+	fmt.Println(now.AddDate(0, 0, *days))
 }
