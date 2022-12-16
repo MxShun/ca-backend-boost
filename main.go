3c0fb0153375127570f007671839e814b085562b
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-16 14:27:22 +0900

    1

--- main.go
+++ main.go
@@ -1,50 +1,14 @@
 package main
 
-import (
-	"golang.org/x/image/draw"
-	"image"
-	"image/png"
-	"os"
-	"path/filepath"
-)
+import "net/http"
 
 func main() {
-	filePath := "io/img.png"
-
-	// ファイルの拡張値を取得
-	ext := filepath.Ext(filePath)
-
-	f, err := os.Open(filePath)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body := []byte("<html><head><title>Go web service</title></head></html>")
+		w.Write(body)
+	})
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		panic(err)
-	}
-	defer func(f *os.File) {
-		err := f.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(f)
-
-	i, _, err := image.Decode(f)
-	if err != nil {
-		panic(err)
-	}
-
-	newImage := image.NewRGBA(image.Rect(0, 0, 300, 300))
-
-	draw.CatmullRom.Scale(newImage, newImage.Bounds(), i, i.Bounds(), draw.Over, nil)
-	newFile, err := os.Create("io/output" + ext)
-	if err != nil {
-		panic(err)
-	}
-	defer func(newFile *os.File) {
-		err := newFile.Close()
-		if err != nil {
-			panic(err)
-		}
-	}(newFile)
-
-	if err := png.Encode(newFile, newImage); err != nil {
 		panic(err)
 	}
 }
