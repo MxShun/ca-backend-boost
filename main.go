d5e382a3bde68f80dd55803cecc029b47550925d
MxShun <16466322+MxShun@users.noreply.github.com>
2022-12-16 14:35:04 +0900

    2

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		body := []byte("<html><head><title>Go web service</title></head></html>")
+		body := []byte("<html><head><title>Go web service</title></head><body>HTMLのトップページです</body></html>")
+		w.Write(body)
+	})
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		body := []byte("<html><head><title>Go web service</title></head><body>Pong</body></html>")
 		w.Write(body)
 	})
 	err := http.ListenAndServe(":8080", nil)
